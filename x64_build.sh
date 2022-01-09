20aae5411fff8487ec4d5f58c0bda34690ea0884
Gregg Shick <gregg_shick@fastmail.fm>
2022-01-09 20:54:15 +0000

    updated build script

---
+++ x64_build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt install git fakeroot build-essential tar ncurses-dev tar xz-utils libssl-dev bc stress python3-distutils libelf-dev 
+sudo apt install linux-headers-$(uname -r) bison flex libncurses5-dev util-linux net-tools linux-tools-$(uname-r) exuberant-ctags cscope
+sudo apt install sysfsutils curl perf-tools-unstable gnuplot rt-tests indent tree psmisc smem libnuma-dev numactl 
+sudo apt install hwloc bpfcc-tools sparse flawfinder cppcheck tuna default-jre trace-cmd virt-what
+
+
