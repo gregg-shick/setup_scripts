cba6251849766dd64f6436e52438f17e8bc40fd5
Gregg Shick <gregg.shick@hpe.com>
2022-01-10 02:17:17 +0000

    arm stuff

---
+++ arm_build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt install -qy autoconf automake bison bzip2 cmake gettext git gperf help2man libncurses5-dev libstdc++6 libtool libtool-bin make 
+sudo apt install -qy patch python3-dev rsync texinfo unzip wget xz-utils
+
